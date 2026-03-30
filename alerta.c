1dacfef9b03f8341be4d3aaf2b89287ed5a6cc4a
Levi Maciel <l3trol007@gmail.com>
2026-03-29 21:25:12 -0300

    primeira tentativa

---
+++ alerta.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void emitir_alerta(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        printf("BIP!\n");
+    }
+}
---
+++ alerta.h
@@ -0,0 +1,6 @@
+#ifndef ALERTA_H
+#define ALERTA_H
+
+void emitir_alerta(int a);
+
+#endif
